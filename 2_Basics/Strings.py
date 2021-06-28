20cc830e9a06c19fe6f534f660a9b34e9126ba9d
TareqJudehGithub <tareq.judeh@outlook.com>
2021-06-29 01:52:15 +0200

    Uploading Introduction chapter, and Basics completed lessons and exercises.

---
+++ 2_Basics/Strings.py
@@ -0,0 +1,98 @@
+"""
+Strings str()
+ - String is a peace of text. Example: "Hello, world!"
+ - Strings are immutable ordered sequence of characters.
+ - String Concatenation.
+    - Adding string together
+    - Only string values can be concat together
+    - We can convert other data types using Type Conversion.
+ - Escape Sequences
+    - \'s enables us to add quotations inside strings.
+        Example: 'Name\'s John '
+    - \t add a tab spacing.  Example: "Column1\t Column2\t Column3\t"
+    - \n Adds a new line.   "Thank you\n Have a lovely day!"
+
+ - formatted Strings
+    greet = 'Hi {0}! You are {1} old.'.format(account_name, age)
+     - We could also ignore indexing and the code will would still work.
+    greet_2 = f'Hi {account_name}! Your are {age} old.'
+     - f'string' is more clean and is the way to go I'd say.
+
+ - String Indexing (Slicing)
+    string[start, stop, optional: step over]
+
+ - Strings are immutable. Strings contents cannot be edited or changed.
+    name = 'john'
+    name[0] = 'J'  >>> TypeError
+    The only way to change a string value, is to reassign a new value (name = 'John')
+
+"""
+print(type("Hello"))
+
+username = "tj.coder"
+password = "Pa$$@"
+long_string = """
+WOW
+o o
+/\
+"""
+
+first_name = 'john'
+last_name = 'smith'
+
+# string concatenation:
+
+print('string concatenation')
+full_name = first_name + "" + last_name
+full_name_f = f"{first_name} {last_name}"
+print(full_name_f, end='\n')
+
+# Type Conversion
+edit_username = username + str(75)
+print(edit_username)
+
+# Escape Sequences
+print("Escape Sequences")
+print("It\"s going to rain today!")
+print("Column1\tColumn2\tColumn3\t")
+print("Thank you\nHave a lovely day!")
+
+# Formatted Strings
+print('Formatted Strings')
+account_name = 'john'
+age = 45
+greet = 'Hi {}! You are {} old.'.format(account_name, age)
+print(greet, end='\n')
+
+# Or we could use f''
+greet_2 = f'Hi {account_name}! Your are {age} old.'
+print(greet_2, end='\n')
+
+
+print("String Indexing (Slicing)")
+# Strings are ordered sequence of character
+student_name = 'sarah cole'
+# Index:        0123456789
+
+
+print(student_name[0:5], end='\n')
+# >>> Sarah
+
+print(student_name[::1])     # start at 0: stop after last index
+
+print(student_name[-1])      # return the last index from the right
+print(student_name[-2])      # return the 2nd index from the right
+print(student_name[::-1])    # reverse the string order
+
+print('\n')
+print('String Methods', '\n')
+
+print('title()')
+print(student_name.title(), '\n')     # Capitalize the first letter of every string
+
+print('find()')
+print(student_name.find('c'), '\n')   # returns the first occurrence it finds and stopped the search.
+
+print('replace()')
+print(student_name.replace('cole', 'coleman').title())
+
---
+++ 2_Basics/variables.py
@@ -0,0 +1,46 @@
+"""
+Variables
+ - Variables are a way for us to store information.
+ - We assign (bind) values to variables. Example: number = 10
+ - Variables values are stored in memory in a form of binary.
+ - Variables in Python are snake_case.  example: first_name
+ - Variables must start with lowercase or underscore (_)
+ - Variables cannot start with a number.    wrong: 3rd_player
+ - Variables do not accept spaces.          wrong: first name
+ - Variables cannot override keyword reserved to Python. Examples: print, input, int.
+ - Variables should be descriptive.
+ - Variables can be re-assigned. example: number = 10   addition = 10 + number
+ - Constants should not be changed, and should be declared in uppercase. Example: PI = 3.14
+ - danders variable __ are Python reserved, and should be left alone.
+ - A statement is an entire line of code, that perform some sort of action. example: score = 10 + 15
+ - An expression is a piece of code that produces a value. The part that follows the assignment
+   operator. - 10 + 15
+ - Augmented assignment operator. (operator)=   example  score += 10
+"""
+first_name = 'john'
+
+# assign variables multiple times
+a, b, c = 1, 2, 3
+
+x, y = 5, 10
+print(x, y)
+
+y, x = x, y
+print(x, y)
+
+# - Expression vs Statements
+
+score = 10
+
+#            This part is the expression
+user_score = 20 + score
+
+# Augmented assignment operator
+
+counter = 0
+counter += 5    # >>> 0 + 5 = 5
+counter -= 2    # >>> 5 - 2 = 3
+counter *= 3    # >>> 3 * 3 = 9
+counter /= 3    # >>> 9/3 =   3
+print(int(counter))
+
