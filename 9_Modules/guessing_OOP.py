73cc8d03bc2179134eb015cd65b9233ede3d81d8
TareqJudehGithub <tareq.judeh@outlook.com>
2021-08-16 18:27:42 +0200

    Updated guessing_game.py code and solved Guessing Game problem using OOP.

---
+++ 9_Modules/guessing_OOP.py
@@ -0,0 +1,55 @@
+from random import randint
+
+
+class GuessingGame:
+    attempts = 3
+
+    def __init__(self):
+        self.guessing_func()
+
+    def restart_game(self):
+        GuessingGame.attempts = 3
+
+        restart = input('Restart game? (y/n) ').lower()
+        if restart == 'y':
+            self.guessing_func()
+
+        else:
+            print('Good bye!')
+            return
+
+    def guessing_func(self):
+        random_num = randint(1, 10)
+
+        print(f'No. to guess {random_num}')
+        while True:
+            try:
+                guess = int(input('Guess a number from 1-10: '))
+                if guess < 1 or guess > 10:
+                    print('Input must be within range')
+                    continue
+
+            except ValueError as err:
+                print(f'Input must be an integer\n{err}')
+
+            else:
+                # winning condition
+                if guess == random_num:
+                    print(f'Correct! you\'ve guessed {random_num}')
+                    return self.restart_game()
+
+                else:
+                    # wrong guess, and update attempts left and display it to the user
+                    GuessingGame.attempts -= 1
+                    print('Wrong guess!')
+                    print(f'Remaining attempts = {GuessingGame.attempts}')
+
+                    # ask user to restart game
+                    if GuessingGame.attempts == 0:
+                        print('Game Over!')
+                        return self.restart_game()
+
+                    continue
+
+
+GuessingGame()
--- 9_Modules/guessing_game.py
+++ 9_Modules/guessing_game.py
@@ -4,43 +4,67 @@ from random import randint
 # Build a guessing game 1 - 10
 
 # instantiate two variables which will serve as randint function arguments
-user_input = sys.argv[1]
+# user_input = sys.argv[1]
 
+# constants
 attempts = 3
-num_to_guess = randint(1, 10)
-print(f'Number to guess: {num_to_guess}')
 
-while True:
+# ask the user to restart game
+def play_again():
+    global attempts
     try:
-        guess = int(input('Guess a number 1-10: '))
-        print(guess)
-        print(f'Attempts left: {attempts}')
-        if 1 <= guess <= 10:
-            print('All good!')
-        else:
-            print('Your input should be between the numbers 1 - 10')
-            continue
+        play = str(input('Play again? (y/n) ').lower())
 
     except ValueError as err:
-        print(f'Your input should only be an integer.\n {err}')
-        continue
+        print(f'Input must me a string only\n{err}')
 
     else:
-        if num_to_guess == guess:
-            print(f'Congratulations!\nYou\'ve got the correct number! which is {num_to_guess}')
+        if play == 'y':
+            attempts = 3
+            guessing_game()
         else:
-            attempts -= 1
-            print('Oh well.. you guessed it wrong this time.. but don\'t give up!')
+            print('Good Bye!')
+
+
+# guessing game
+def guessing_game():
+    global attempts
+    print('Welcome to the guessing game!\n')
+
+    while True:
+        try:
+            num_to_guess = randint(1, 10)
+            print(f'Number to guess: {num_to_guess}')
+
+            guess = int(input('Guess a number 1-10: '))
+            print(guess)
             print(f'Attempts left: {attempts}')
+            if 1 <= guess <= 10:
+                print('All good!')
+            else:
+                print('Your input should be between the numbers 1 - 10')
+                continue
 
-            if attempts == 0:
-                print('Game Over! Good Bye!')
+        except ValueError as err:
+            print(f'Your input should only be an integer.\n {err}')
+            continue
 
-                play_again = input('Play again? (y/n) ').lower()
-                if play_again == 'y':
-                    attempts = 3
-                    continue
+        else:
+            if num_to_guess == guess:
+                print(f'Congratulations!\nYou\'ve got the correct number! which is {num_to_guess}!')
+                return play_again()
 
-                else:
-                    print('Good Bye!')
+            else:
+                attempts -= 1
+                print('Oh well.. you guessed it wrong this time.. but don\'t give up!')
+                print(f'Attempts left: {attempts}')
+
+                if attempts == 0:
+                    print('Game Over!')
                     break
+
+    return play_again()
+
+
+if __name__ == '__main__':
+    guessing_game()
