9ff87b3b1ddb9d1108eedb16173ee506391ff51c
TareqJudehGithub <tareq.judeh@outlook.com>
2021-09-24 15:43:30 +0200

    Updated Password Checker documentation file notes.txt

--- 15_Scripting/password_checker/notes.py
+++ 15_Scripting/password_checker/notes.py
@@ -19,4 +19,14 @@ modules used
     - more info on https://haveibeenpwned.com/API/v2
 
 
+1. built Pwned Passwords API response function.
+2. built a function to hash user plain password input.
+3. extracted the first 5 digits from the hashed password, and used them
+   as a query for Pwned Passwords API func.
+3. built a function that compare the API response with hashed password for
+   vulnerabilities.
+4. built a main func, where users get to input their password for checking.
+   If vulnerabilities were found, the program return a message for the user
+   with the total number of breaches found on his/her password entered.
 """
+
